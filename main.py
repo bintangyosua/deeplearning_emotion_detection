ed4e4a138608b70945d6640678576eb53a10bea8
Bintang <itzbintangyosua@gmail.com>
2024-10-22 09:23:04 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import nltk
+import streamlit as st
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pickle
+import re
+import string
+
+from tensorflow import keras
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from tensorflow.keras.utils import pad_sequences
+
+model = tf.keras.models.load_model('models/model_0.84.keras')
+with open('models/tokenizer.pickle', 'rb') as handle:
+  tokenizer = pickle.load(handle)
+
+def load_stopwords():
+    try:
+        with open('stopwords.txt', 'r') as f:
+            return set(word.strip() for word in f.readlines())
+    except FileNotFoundError:
+        # If the file doesn't exist, download the stopwords and save them
+        nltk.download('stopwords')
+        stop_words = set(stopwords.words('english'))
+        with open('stopwords.txt', 'w') as f:
+            for word in stop_words:
+                f.write(f"{word}\n")
+        return stop_words
+
+# Load stopwords
+stop_words = load_stopwords()
+
+# Streamlit UI
+st.title("Emotion Classification")
+user_input = st.text_area('Please enter your text in English')
+
+# Preprocessing function
+def text_preprocess(text):
+    # Remove HTML
+    text = re.sub(pattern, '', text)
+    # Lowercase
+    text = text.lower()
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', punctuation))
+    # Tokenization and stemming, excluding stopwords
+    text = text.split()
+    text = [ps.stem(word) for word in text if word not in stop_words]
+    return ' '.join(text)
+
+# Predefined patterns and stemmer
+pattern = re.compile('<.*?>')  # Pattern for removing HTML tags
+punctuation = string.punctuation  # Extracting punctuation
+ps = PorterStemmer()  # Creating a PorterStemmer object
+
+if st.button("Emotion Prediction"):
+    if user_input:
+        processed_texts = text_preprocess(user_input)
+        sequence = tokenizer.texts_to_sequences([processed_texts])
+        padded_sequence = pad_sequences(sequence, maxlen=50, padding='post')
+        prediction = model.predict(padded_sequence)
+        predicted_label = np.argmax(prediction, axis=1)
+        emotion_dict = {
+          0: 'Sadness',
+          1: 'Joy',
+          2: 'Love',
+          3: 'Anger',
+          4: 'Fear',
+          5: 'Surprise'
+        }
+        st.success(f"Detected Emotion: {emotion_dict[predicted_label[0]]}")
+    else:
+        st.error("Please enter your text first.")
